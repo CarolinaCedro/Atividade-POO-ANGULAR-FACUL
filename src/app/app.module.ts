f3d8e5742816a3cc72497ee7c8c9daa3ea3b1e11
CarolinaCedro <yotechgirl@gmail.com>
2023-05-07 16:02:11 -0300

    feat: +++

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
